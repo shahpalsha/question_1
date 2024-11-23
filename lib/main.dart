d2fe87fdca4382595190d7cf06223e363fe5c1d3
shahpalsha <shahpalsha@gmail.com>
2024-11-23 17:24:09 +0530

    Quetion 1 commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  //quetion 1
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
